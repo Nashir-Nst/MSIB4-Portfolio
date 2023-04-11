b18ffa58368a9e6c9f2bbd084080fdfc97ea0c31
Muhammad Nashir Nasution <72939850+Nashir-Nst@users.noreply.github.com>
2023-04-12 06:50:36 +0700

    Tugas Portfolio

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        Copyright  &copy; 2023 <strong><span>Nashir Nasution</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Design by <a href="">Nashir</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
